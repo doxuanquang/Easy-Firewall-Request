9b2371e39b1ea0e59cffc56369dec490c38b74f5
doxuanquang <doxuanquang1989@gmail.com>
2018-05-18 17:08:45 +0800

    Add files via upload

--- subnet.py
+++ subnet.py
@@ -1,15 +1,20 @@
+
 import math
-import reg
+import re
 from conversion import netmask
 
+"""
 network = network.split("/")  #split network & mask
 network_id = network[0]
 network_mask = network[1]
+"""
 
-if re.match(r"[0-9][0-9]", network_mask):
-  network_mask = network_mask
-else if re.match (r"[0-9][0-9][0-9]'\.'[0-9][0-9][0-9]'\.'[0-9][0-9][0-9]'\.'[0-9][0-9][0-9]"):
-  network_mask = netmask(network_mask)
-else print("Wrong Mask!")
+network_mask = "255.255.255.256"
+if len(network_mask) <3:
+    network_mask = network_mask
+elif re.match (r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$", network_mask) != None:
+    network_mask = netmask(network_mask)
+else:
+    print("Wrong Mask!")
 
-
+print(network_mask)
